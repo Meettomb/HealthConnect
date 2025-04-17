dd7394847a7b297edd2436faeef09fe770eec556
Meet Patel <patelmeet23032005>
2025-04-17 22:24:30 +0530

    Validation add on Sign_up page

---
+++ HealthConnect/Models/Blog_Table.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HealthConnect.Models
+{
+    public class Blog_Table
+    {
+        [Key]public int blog_id { get; set; }
+        public int user_id { get; set; }
+        public string blog_image { get; set; }
+        public string blog_title { get; set; }
+        public string blog_content { get; set; }
+        public DateOnly blog_date { get; set; }
+    }
+}
